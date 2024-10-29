f18f200244eed45641f7aa32c9041f51e8677624
Blake Voyles <sblakevoyles@gmail.com>
2024-10-29 12:27:05 -0400

    committing db schema

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,27 @@
 /** @type {import('next').NextConfig} */
+import { PrismaClient } from '@prisma/client'
+
 const nextConfig = {};
 
 export default nextConfig;
+
+
+const prisma = new PrismaClient()
+// use `prisma` in your application to read and write data in your DB
+
+async function main() {
+    const user = await prisma.user.create({
+        data: {
+            email: "blakevoyles2002@gmail.com",
+            firstName: "Blake",
+            lastName: "Voyles",
+            profilePic : " ",
+            userName: "blakeV"
+            
+        }
+    });
+    console.log(user)
+}
+
+
+main()

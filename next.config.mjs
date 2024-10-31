cc8c9acb60aa27c35aa794640b5c63a01461e5e1
StephenSulimani <stephensulimani@gmail.com>
2024-10-31 11:48:32 -0400

    :tada: changed database_url and tested prisma

--- next.config.mjs
+++ next.config.mjs
@@ -1,27 +1,25 @@
 /** @type {import('next').NextConfig} */
-import { PrismaClient } from '@prisma/client'
+// import { PrismaClient } from '@prisma/client'
 
 const nextConfig = {};
 
 export default nextConfig;
 
+// const prisma = new PrismaClient()
+// // use `prisma` in your application to read and write data in your DB
 
-const prisma = new PrismaClient()
-// use `prisma` in your application to read and write data in your DB
+// async function main() {
+//     const user = await prisma.user.create({
+//         data: {
+//             email: "blakevoyles2002@gmail.com",
+//             firstName: "Blake",
+//             lastName: "Voyles",
+//             profilePic : " ",
+//             userName: "blakeV"
 
-async function main() {
-    const user = await prisma.user.create({
-        data: {
-            email: "blakevoyles2002@gmail.com",
-            firstName: "Blake",
-            lastName: "Voyles",
-            profilePic : " ",
-            userName: "blakeV"
-            
-        }
-    });
-    console.log(user)
-}
+//         }
+//     });
+//     console.log(user)
+// }
 
-
-main()
+// main()
--- src/app/api/ping/route.ts
+++ src/app/api/ping/route.ts
@@ -1,3 +1,21 @@
+import { prisma } from '@/app/lib';
+
 export async function GET() {
+    const user = await prisma.user.findFirst({
+        where: {
+            firstName: 'Stephen',
+        },
+    });
+
+    const post = await prisma.post.create({
+        data: {
+            price: 55,
+            description: 'random listing',
+            pic: 'n/a',
+            userId: user!.id,
+        },
+    });
+    console.log(user);
+    console.log(post);
     return Response.json({ status: 1, message: 'Pong!' });
 }
